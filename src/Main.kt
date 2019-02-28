99b88db697e6c2712f8755ea1f3ef21dbacce716
Bassam <BassamA.Mansour@gmail.com>
2019-02-28 20:12:53 +0200

    test cases

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,29 @@
 import java.io.File
+import java.util.*
+import kotlin.collections.HashMap
 
 
-fun main() {
-    val lines = File("a.txt").readLines()
+fun main(args: Array<String>) {
+    val lines = File("a.txt").readLines().toTypedArray()
 
-    val iterator = lines.iterator()
+    val n = lines[0].toInt()
 
-    val firstLine = iterator.next()
+    val horizontalPhotos = LinkedList<Photo>()
+    val verticalPhotos = LinkedList<Photo>()
+    val photosHorizontalMap = HashMap<String, List<Photo>>()
+    val photosVerticalMap = HashMap<String, List<Photo>>()
 
-    val args = firstLine.split(" ")
+    repeat(n) {
 
-    for (line in iterator) {
+        val photo = getPhotoFromLine(lines[it], it)
 
 
     }
 }
+
+fun getPhotoFromLine(line: String, id: Int): Photo {
+    val photoArgs = line.split(" ")
+    return Photo(id, photoArgs[0][0], photoArgs.subList(2, photoArgs.size))
+}
+
+data class Photo(var id: Int, var orientation: Char, var tags: List<String>)
