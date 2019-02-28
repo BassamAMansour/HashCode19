127afc44e3c56523c6f951e1666367620228ce6c
Bassam <BassamA.Mansour@gmail.com>
2019-02-28 20:25:48 +0200

    test cases

--- src/Main.kt
+++ src/Main.kt
@@ -10,14 +10,38 @@ fun main(args: Array<String>) {
 
     val horizontalPhotos = LinkedList<Photo>()
     val verticalPhotos = LinkedList<Photo>()
-    val photosHorizontalMap = HashMap<String, List<Photo>>()
-    val photosVerticalMap = HashMap<String, List<Photo>>()
+    val horizontalMap = HashMap<String, LinkedList<Photo>>()
+    val verticalMap = HashMap<String, LinkedList<Photo>>()
 
     repeat(n) {
 
-        val photo = getPhotoFromLine(lines[it], it)
+        val photo = getPhotoFromLine(lines[it + 1], it)
+
+        when (photo.orientation) {
+            'H' -> {
+                horizontalPhotos.add(photo)
+                addTagsToMap(horizontalMap, photo)
+            }
+
+            'V' -> {
+                verticalPhotos.add(photo)
+                addTagsToMap(verticalMap, photo)
+            }
+        }
+    }
 
 
+}
+
+fun addTagsToMap(map: HashMap<String, LinkedList<Photo>>, photo: Photo) {
+    for (tag in photo.tags) {
+        if (map.containsKey(tag)) {
+            map[tag]!!.add(photo)
+        } else {
+            val tags = LinkedList<Photo>()
+            tags.add(photo)
+            map[tag] = tags
+        }
     }
 }
 
@@ -27,3 +51,5 @@ fun getPhotoFromLine(line: String, id: Int): Photo {
 }
 
 data class Photo(var id: Int, var orientation: Char, var tags: List<String>)
+data class Slide(var tags: HashSet<String>, var photos: LinkedList<Photo>)
+data class SlideShow(var slides: LinkedList<Slide>)
