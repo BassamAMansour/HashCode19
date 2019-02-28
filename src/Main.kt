6adbe9db526d27d94b277388dae0e8589a4816aa
Bassam <BassamA.Mansour@gmail.com>
2019-02-28 19:56:07 +0200

    test cases

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,17 @@
 import java.io.File
 
 
+fun main() {
+    val lines = File("a.txt").readLines()
+
+    val iterator = lines.iterator()
+
+    val firstLine = iterator.next()
+
+    val args = firstLine.split(" ")
+
+    for (line in iterator) {
 
 
-
-fun main(){
-    val lines = File("data.txt").readLines()
-
-    val iterator =lines.iterator()
-    
+    }
 }
